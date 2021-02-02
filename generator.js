5b8596ac630788983d16b53537f7fcfc6e442368
Matthew <mmackinnon01@qub.ac.uk>
2021-02-02 11:23:45 +0000

    Created message generator

---
+++ generator.js
@@ -0,0 +1,16 @@
+
+let generator = {
+    verb: ['refracts', 'resonates', 'diffracts', 'decays', 'oscillates'],
+    noun1: ['sun', 'ball', 'proton', 'electron', 'light', 'neutron'],
+    noun2: ['glass', 'atom', 'barrier', 'bend'],
+
+    selectRandomWord(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    },
+
+    generateRandomMessage() {
+        return `The ${this.selectRandomWord(this.noun1)} ${this.selectRandomWord(this.verb)} while the ${this.selectRandomWord(this.noun2)} ${this.selectRandomWord(this.verb)}!`;
+    }
+}
+
+console.log(generator.generateRandomMessage());
